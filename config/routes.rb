9018a682b22d3fbd7a5d3d765a6f72c977892632
Gor <7495500@gmail.com>
2024-10-17 22:18:30 +0300

    Add locales configs with i18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  devise_for :users
-  resources :posts do
-    resources :comments, only: [:create]
+
+  scope "(:locale)", locale: /en|ru/ do
+    devise_for :users
+
+    resources :posts do
+      resources :comments, only: [:create]
+    end
   end
 end
