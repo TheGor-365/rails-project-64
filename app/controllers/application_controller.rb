4412e4fcd7bebe0f12e8dde455832ae545413ed4
Gor <7495500@gmail.com>
2024-11-24 17:04:55 +0300

    Fix application_controller bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def switch_locale(&)
     locale = params[:locale] || I18n.default_locale
-    I18n.with_locale(locale, &action)
+    I18n.with_locale(locale, &)
   end
 
   def default_url_options
