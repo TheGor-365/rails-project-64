ffce59f4861e29fabf7f99d05870dc4d59e05b0a
Gor <7495500@gmail.com>
2025-02-27 00:16:09 +0300

    Modify application_controller.rb, switch_locale handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :switch_locale
+  around_action :switch_locale
 
-  def switch_locale(&)
+  def switch_locale(&action)
     locale = params[:locale] || I18n.default_locale
-    I18n.with_locale(locale, &)
+    I18n.with_locale(locale, &action)
   end
 
   def default_url_options
