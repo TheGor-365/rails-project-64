f190e213528913712a992344ac3996aa961f3eef
Gor <7495500@gmail.com>
2025-08-21 23:55:24 +0300

    render: run db:prepare & db:seed on start, log commands

--- bin/render-start.sh
+++ bin/render-start.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 set -e
+set -euo pipefail
+set -x
 
-until bundle exec rails db:version >/dev/null 2>&1; do
-  echo "Waiting for database..."
-  sleep 2
-done
-
-bundle exec rails db:migrate
+bundle exec rails db:prepare
 bundle exec rails db:seed
 
 exec bundle exec puma -C config/puma.rb
