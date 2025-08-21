b0fd9e142f94deed3246d8382342895ce2b33b11
Gor <7495500@gmail.com>
2025-08-21 21:11:13 +0300

    Modify ci.yml

--- db/migrate/20250821174916_add_index_on_post_comments_ancestry.rb
+++ db/migrate/20250821174916_add_index_on_post_comments_ancestry.rb
@@ -2,6 +2,6 @@
 
 class AddIndexOnPostCommentsAncestry < ActiveRecord::Migration[7.2]
   def change
-    add_index :post_comments, :ancestry
+    add_index :post_comments, :ancestry, if_not_exists: true
   end
 end
