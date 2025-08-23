77cf7e86c3f9b75a8dd6d7c1938b82d471a54b64
Gor <7495500@gmail.com>
2025-08-23 21:25:48 +0300

    Fix linter bugs 8

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,8 +6,7 @@ Rails.application.configure do
   config.generators.after_generate do |files|
     parsable_files = files.filter { |file| file.end_with?('.rb') }
     unless parsable_files.empty?
-      system("bundle exec rubocop -A --fail-level=E #{parsable_files.shelljoin}",
-        exception: true)
+      system("rubocop -A --fail-level=E #{parsable_files.shelljoin}", exception: true)
     end
   end
 
