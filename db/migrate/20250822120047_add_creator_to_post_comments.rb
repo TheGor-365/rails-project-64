414fae189ac51b0cc319595a014b219bc640637f
Gor <7495500@gmail.com>
2025-08-24 13:43:31 +0300

    Modify db

--- db/migrate/20250822120047_add_creator_to_post_comments.rb
+++ db/migrate/20250822120047_add_creator_to_post_comments.rb
@@ -2,6 +2,15 @@
 
 class AddCreatorToPostComments < ActiveRecord::Migration[7.2]
   def change
-    add_reference :post_comments, :creator, null: false, foreign_key: { to_table: :users }
+    unless column_exists?(:post_comments, :creator_id)
+      add_reference :post_comments,
+                    :creator,
+                    null: true,
+                    foreign_key: { to_table: :users }
+    else
+      change_column_null :post_comments, :creator_id, true
+      add_foreign_key :post_comments, :users, column: :creator_id unless foreign_key_exists?(:post_comments, :users, column: :creator_id)
+      add_index :post_comments, :creator_id unless index_exists?(:post_comments, :creator_id)
+    end
   end
 end
