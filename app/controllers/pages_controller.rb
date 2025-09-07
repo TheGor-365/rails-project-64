1057cad53ec9b5ecef7be4b7656196edcb79d0a0
Gor <7495500@gmail.com>
2025-09-07 11:42:47 +0300

    Modify routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
+# # frozen_string_literal: true
+
+# class PagesController < ApplicationController
+#   def home
+#     @posts = Post.includes(:creator, :category, :likes).order(created_at: :desc)
+#   end
+# end
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-  def home
-    @posts = Post.includes(:creator, :category, :likes).order(created_at: :desc)
+  def posts
+    @posts = Post.includes(:category, :creator).order(id: :desc)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # БАЗОВЫЕ без :locale — под школьные тесты
-  resources :posts, only: %i[new create show] do
-    resources :comments, only: :create
-    resources :likes, only: %i[create destroy]
-  end
-
-  # Локализованные алиасы (UX по /en/...), плоские лайки оставляем для текущего UI
-  scope '(:locale)' do
-    resources :posts, only: %i[new show create], as: :localized_posts
-    resources :likes, only: %i[create destroy]
-
-    root 'pages#home'
+  scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
+    root 'pages#posts'
     devise_for :users
+
+    resources :posts, only: %i[new create show] do
+      resources :comments, only: :create
+      resources :likes, only: %i[create destroy]
+    end
+    resources :likes, only: %i[create destroy]
   end
 end
