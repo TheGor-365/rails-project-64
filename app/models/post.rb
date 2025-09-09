a6afbfb906f83badff57cc468f6bd8fddb298d0a
Gor <7495500@gmail.com>
2025-09-09 22:39:32 +0300

    Remove not used likes

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,6 @@ class Post < ApplicationRecord
 
   has_many :comments, class_name: 'PostComment', dependent: :destroy
   has_many :likes, class_name: 'PostLike', dependent: :destroy
-  has_many :likers, through: :likes, source: :user
 
   validates :title, length: { minimum: 4, maximum: 255 }
   validates :title, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,5 @@ class User < ApplicationRecord
 
   has_many :posts, class_name: 'Post', foreign_key: :creator_id, inverse_of: :creator, dependent: :destroy
   has_many :likes, class_name: 'PostLike', dependent: :destroy
-  has_many :liked_posts, through: :likes, source: :post
   has_many :comments, class_name: 'PostComment', dependent: :destroy
 end
