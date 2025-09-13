dca50e34fa2cc9e10c9af1ac5b7e119252a029a5
Gor <7495500@gmail.com>
2025-09-13 17:25:08 +0300

    Modify likes_tests

--- test/controllers/likes_controller_test.rb
+++ test/controllers/likes_controller_test.rb
@@ -5,22 +5,22 @@ require 'test_helper'
 class LikesControllerTest < ActionDispatch::IntegrationTest
   setup do
     sign_in users(:one)
-    @post = posts(:one)
-    PostLike.where(user: users(:one), post: @post).delete_all
+    @post_with_like    = posts(:one)
+    @post_without_like = posts(:two)
   end
 
-  test 'POST /posts/:post_id/likes likes a post' do
+  test 'POST /posts/:post_id/likes likes a post without existing like' do
     assert_difference('PostLike.count', +1) do
-      post post_likes_url(@post)
+      post post_likes_url(@post_without_like)
     end
     follow_redirect!
     assert_response :success
   end
 
   test 'DELETE /posts/:post_id/likes/:id unlikes a post' do
-    like = PostLike.create!(user: users(:one), post: @post)
+    like = PostLike.find_by!(user: users(:one), post: @post_with_like)
     assert_difference('PostLike.count', -1) do
-      delete post_like_url(@post, like)
+      delete post_like_url(@post_with_like, like)
     end
     follow_redirect!
     assert_response :success
