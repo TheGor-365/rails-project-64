221631e1118ef003cecc2fe932169412bb943cda
Gor <7495500@gmail.com>
2025-09-13 17:54:23 +0300

    Modify likes_tests, add variable for user

--- test/controllers/likes_controller_test.rb
+++ test/controllers/likes_controller_test.rb
@@ -4,7 +4,8 @@ require 'test_helper'
 
 class LikesControllerTest < ActionDispatch::IntegrationTest
   setup do
-    sign_in users(:one)
+    @user = users(:one)
+    sign_in @user
     @post_with_like    = posts(:one)
     @post_without_like = posts(:two)
   end
@@ -14,7 +15,7 @@ class LikesControllerTest < ActionDispatch::IntegrationTest
       post post_likes_url(@post_without_like)
     end
 
-    created = PostLike.find_by(user: users(:one), post: @post_without_like)
+    created = PostLike.find_by(user: @user, post: @post_without_like)
     assert { created }
 
     follow_redirect!
@@ -22,7 +23,7 @@ class LikesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'DELETE /posts/:post_id/likes/:id unlikes a post' do
-    like = PostLike.find_by!(user: users(:one), post: @post_with_like)
+    like = PostLike.find_by!(user: @user, post: @post_with_like)
 
     assert_difference('PostLike.count', -1) do
       delete post_like_url(@post_with_like, like)
