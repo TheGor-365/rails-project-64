9d3b558b6f4f6e4ef47c6d45c12099f24b14326a
Gor <7495500@gmail.com>
2025-09-13 17:07:01 +0300

    Add show for posts tests

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+
 class PostsControllerTest < ActionDispatch::IntegrationTest
   setup do
     sign_in users(:one)
     @category = categories(:one)
+    @post = posts(:one)
+  end
+
+  test 'GET /posts/:id shows post' do
+    get post_url(@post)
+    assert_response :success
+    assert_includes @response.body, @post.title
   end
 
   test 'GET /posts/new' do
